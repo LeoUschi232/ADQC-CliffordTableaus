585cb90369b26faad88603673e1e540dfd455802
Leonard Uscinowicz <leouscin@gmail.com>
2024-11-09 00:18:08 +0100

    Changed language to C++

---
+++ src/template.cpp
@@ -0,0 +1,5 @@
+//
+// Created by leouscin on 11/9/24.
+//
+
+#include "template.h"
---
+++ src/template.h
@@ -0,0 +1,16 @@
+//
+// Created by leouscin on 11/9/24.
+//
+
+#ifndef ADQC_TEMPLATE_H
+#define ADQC_TEMPLATE_H
+
+
+class
+
+template {
+
+};
+
+
+#endif //ADQC_TEMPLATE_H
